a23a078aa9f1e755f899583daec64d06bb615b7a
Gregory Szaro <gregoryszaro@gmail.com>
2023-05-15 19:32:19 -0400

    completes liker

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Select the error modal and add the .hidden class
+const errorModal = document.querySelector('#modal')
+errorModal.classList.add('hidden')
+
+// Select all the hearts and add a click event listener to each one
+const hearts = document.querySelectorAll('.like-glyph')
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    // Invoke mimicServerCall to simulate a server request
+    mimicServerCall()
+      .then(() => {
+        // Change the heart to a full heart and add the .activated-heart class
+        heart.innerText = FULL_HEART
+        heart.classList.add('activated-heart')
+      })
+      .catch((error) => {
+        // Display the error modal and error message
+        errorModal.classList.remove('hidden')
+        errorModal.querySelector('#modal-message').innerText = error
+        // Hide the error modal after 3 seconds
+        setTimeout(() => {
+          errorModal.classList.add('hidden')
+        }, 3000)
+      })
+  })
+})
 
 
 
